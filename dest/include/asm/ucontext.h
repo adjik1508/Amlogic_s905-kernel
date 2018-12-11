e6f325ac0735af6b0d0fa6dc64fabb090b4e5d24
balbes150 <balbes-150@yandex.ru>
2018-12-11 11:09:51 +0300

    v20181211

--- dest/include/asm/ucontext.h
+++ dest/include/asm/ucontext.h
@@ -14,4 +14,20 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <asm-generic/stat.h>
+#ifndef __ASM_UCONTEXT_H
+#define __ASM_UCONTEXT_H
+
+#include <linux/types.h>
+
+struct ucontext {
+	unsigned long	  uc_flags;
+	struct ucontext	 *uc_link;
+	stack_t		  uc_stack;
+	sigset_t	  uc_sigmask;
+	/* glibc uses a 1024-bit sigset_t */
+	__u8		  __unused[1024 / 8 - sizeof(sigset_t)];
+	/* last for future expansion */
+	struct sigcontext uc_mcontext;
+};
+
+#endif /* __ASM_UCONTEXT_H */
--- dest/include/linux/version.h
+++ dest/include/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 267014
+#define LINUX_VERSION_CODE 267015
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
