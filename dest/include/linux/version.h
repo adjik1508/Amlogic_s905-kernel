054f512e6e42ff25ca5b0dc56a52de40da34575d
balbes150 <balbes-150@yandex.ru>
2019-01-09 16:07:13 +0300

    v20190109

--- dest/include/linux/version.h
+++ dest/include/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 267015
+#define LINUX_VERSION_CODE 267021
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
--- drivers/gpu/drm/meson/meson_crtc.c
+++ drivers/gpu/drm/meson/meson_crtc.c
@@ -123,8 +123,6 @@ static void meson_crtc_atomic_enable(struct drm_crtc *crtc,
 		meson_crtc_enable(crtc);
 
 	priv->viu.osd1_enabled = true;
-
-	drm_crtc_vblank_on(crtc);
 }
 
 static void meson_crtc_atomic_disable(struct drm_crtc *crtc,
@@ -133,9 +131,9 @@ static void meson_crtc_atomic_disable(struct drm_crtc *crtc,
 	struct meson_crtc *meson_crtc = to_meson_crtc(crtc);
 	struct meson_drm *priv = meson_crtc->priv;
 
-	DRM_DEBUG_DRIVER("\n");
-
 	drm_crtc_vblank_off(crtc);
+
+	DRM_DEBUG_DRIVER("\n");
 
 	priv->viu.osd1_enabled = false;
 	priv->viu.osd1_commit = false;
